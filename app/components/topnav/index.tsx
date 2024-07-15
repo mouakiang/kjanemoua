3508d72ddd150f86add55cd7d3cb5e4550b0d46e
mouakiang <mouakkia@gmail.com>
2024-07-15 15:37:48 -0400

    Working on topNav

---
+++ app/components/topnav/index.tsx
@@ -0,0 +1,7 @@
+export default function TopNav() {
+    return (
+        <main>
+            <div className="my-4 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-550 to-transparent opacity-50 dark:via-neutral-950"></div>
+        </main>
+    );
+}
