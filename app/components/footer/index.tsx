668da6974fa89bd1219e80f379f660d254b29cd5
mouakiang <mouakkia@gmail.com>
2024-07-25 15:44:01 -0400

    Added social icons and styling to footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,15 +1,51 @@
+'use client';
 import { quattrocento } from '../font/fonts';
+import { SocialIcon } from 'react-social-icons';
 
 export default function Footer() {
     return (
-        <main>
-            <div className="">
-                <h1
-                    className={`${quattrocento.className} flex justify-center items-center h-14`}
-                >
+        <footer className="bg-white text-black py-4">
+            <div className="container mx-auto flex items-center h-10 px-0 mr-12">
+                <h1 className={`${quattrocento.className} mx-auto`}>
                     ©KJaneMoua Photography 2024
                 </h1>
+                <div className="flex space-x-2 ml-auto">
+                    <SocialIcon
+                        url="https://www.facebook.com/kjanemouaphotography"
+                        className="social-icon"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ height: 35, width: 35 }}
+                    />
+                    <SocialIcon
+                        url="https://www.instagram.com/kjanemouaphotography"
+                        className="social-icon"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ height: 35, width: 35 }}
+                    />
+                </div>
             </div>
-        </main>
+            <style jsx>{`
+                .social-icon {
+                    width: 35px;
+                    height: 35px;
+                }
+
+                @media (max-width: 768px) {
+                    .social-icon {
+                        width: 28px;
+                        height: 28px;
+                    }
+                }
+
+                @media (max-width: 480px) {
+                    .social-icon {
+                        width: 24px;
+                        height: 24px;
+                    }
+                }
+            `}</style>
+        </footer>
     );
 }
