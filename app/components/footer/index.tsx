d2f5abadd511f053bec83087a378e80f9a28d69c
mouakiang <mouakkia@gmail.com>
2024-07-26 14:46:27 -0400

    Updated styling for mobile first approach

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -4,48 +4,30 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function Footer() {
     return (
-        <footer className="bg-white text-black py-4">
+        <footer className="bg-white text-black md:py-4">
             <div className="container mx-auto flex items-center h-10 px-0 mr-12">
-                <h1 className={`${quattrocento.className} mx-auto`}>
+                <h1
+                    className={`${quattrocento.className} mx-auto text-xs sm:text-sm md:text-lg`}
+                >
                     ©KJaneMoua Photography 2024
                 </h1>
                 <div className="flex space-x-2 ml-auto">
                     <SocialIcon
                         url="https://www.facebook.com/kjanemouaphotography"
-                        className="social-icon"
+                        className=""
                         target="_blank"
                         rel="noopener noreferrer"
-                        style={{ height: 35, width: 35 }}
+                        style={{ height: 28, width: 28 }}
                     />
                     <SocialIcon
                         url="https://www.instagram.com/kjanemouaphotography"
-                        className="social-icon"
+                        className=""
                         target="_blank"
                         rel="noopener noreferrer"
-                        style={{ height: 35, width: 35 }}
+                        style={{ height: 28, width: 28 }}
                     />
                 </div>
             </div>
-            <style jsx>{`
-                .social-icon {
-                    width: 35px;
-                    height: 35px;
-                }
-
-                @media (max-width: 768px) {
-                    .social-icon {
-                        width: 28px;
-                        height: 28px;
-                    }
-                }
-
-                @media (max-width: 480px) {
-                    .social-icon {
-                        width: 24px;
-                        height: 24px;
-                    }
-                }
-            `}</style>
         </footer>
     );
 }
